a9e13c37862d7ad7cec9f0cbc9767d4985debef4
ShangBin <dkfkgks64@gmail.com>
2022-12-29 16:40:40 +0900

    chore : Add imports for url, path and Set view engine and view path

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import _ from './lib/env_config.js';
 import express from 'express';
 import example from './router/example.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const viewPath = path.join(__dirname, 'view');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', viewPath);
 
 app.use('/example', example);
 app.get("/", (req, res) => res.end("Hello World!"));
