84fcafa502a29d0568a6954510331d9b97697062
LeeShangBin <dkfkgks64@gmail.com>
2023-01-08 19:21:44 +0900

    feature : Implement selectDayFutsalMatch

--- db/select_match.js
+++ db/select_match.js
@@ -1,3 +1,4 @@
+import { query } from './db_helper.js';
 import selectAsFieldFrom from './select_as_field_from.js';
 
 /**
@@ -8,4 +9,21 @@ import selectAsFieldFrom from './select_as_field_from.js';
 const selectFutsalMatch = id => 
     selectAsFieldFrom('futsal_match', 'id', id);
 
-export { selectFutsalMatch };
+/**
+ * dateOfDay 날짜에 startDateTime 시각 이후 시작되는 풋살 경기 정보 레코드를 가져오는 함수
+ * @param {String} dateOfDay - MySQL DATE 형식의 string
+ * @param {Date | String} startDateTime - Date 객체이거나 DateTime 형식의 string
+ * @return {Promise<Array<RowDataPacket>>} 해당 날짜, 해당 시각 이후에 시작하는 경기 정보 레코드 배열
+ */
+const selectDayFutsalMatch= (dateOfDay, startDateTime) =>
+    query('SELECT * FROM futsal_match WHERE DATE(start_time) = ? AND start_time > ?',
+        [
+            dateOfDay,
+            startDateTime
+        ]);
+
+export 
+{ 
+    selectFutsalMatch,
+    selectDayFutsalMatch
+};
