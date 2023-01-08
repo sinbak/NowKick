e54c157d3457d3ab07f0b069ae0f3fc4e676bfac
LeeShangBin <dkfkgks64@gmail.com>
2023-01-08 17:16:53 +0900

    feature : Implement handler for /user/error/400
    Make the handler process user info error page request.

--- router/user.js
+++ router/user.js
@@ -28,4 +28,33 @@ router.get('/:uid', async function (req, res) {
     }
   });
 
+/**
+ * req.query에 fields 속성이 존재하는지 검증하고
+ * fields 값이 유효한 유저 정보 필드명을 쉼표로 구분하여 나열한 문자열인지 검증한다
+ * 검증에 성공하면 fields 값에 명시된 필드명(한글명으로 변환)을 포함하는 에러 페이지로 응답한다
+ * 실패하면 400 Bad Request를 응답한다
+ */
+router.get('/error/400', (req, res, next) =>
+{
+    const userFields = [ 'name', 'stuid', 'department', 'grade', 'gender' ];
+    const userFieldMap = {
+        name : '이름',
+        stuid : '학번',
+        department : '학과',
+        grade : '학년',
+        gender : '성별'
+    };
+
+    if(req.query.fields === undefined || req.query.fields === '')
+        return res.status(400).send('400 잘못된 경로입니다');
+
+    const fieldList = req.query.fields.split(',');
+                 
+    
+    if(fieldList.every(v => userFields.includes(v)) === false)
+        return res.status(400).send('400 잘못된 경로입니다');
+
+    return res.send(`${fieldList.map(v => userFieldMap[v]).join(', ')} 필드가 잘못 입력되었습니다`);
+});
+
 export default router
